e11988fb29bcfb6071b4899ebb4ce374a9d063b1
hieuph2410302-droid <hieuph2410302@usth.edu.vn>
2026-09-25 16:31:26 +0700

    Delete ex6.py

--- ex6.py
+++
@@ -1,4 +0,0 @@
-print("range1 |", ", ".join(map(str, range(7))))
-print("range2 |", ", ".join(map(str, range(1, 13, 3))))
-print("range3 |", ", ".join(map(str, range(5, 0, -1))))
-print("range4 |", ", ".join(map(str, range(6, -3, -2))))
