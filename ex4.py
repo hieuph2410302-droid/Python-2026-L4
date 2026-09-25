f78895e91a35039a84cd688b3849b9105ea7a5aa
hieuph2410302-droid <hieuph2410302@usth.edu.vn>
2026-09-25 16:30:59 +0700

    Delete ex4.py

--- ex4.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number? "))
-if num > 0:
-    sum_divisors = sum([i for i in range(1, num) if num % i == 0])
-    if sum_divisors == num:
-        print(f"{num} is a perfect number")
-    else:
-        print(f"{num} is a NOT perfect number")
-else:
-    print(f"{num} is a NOT perfect number")
