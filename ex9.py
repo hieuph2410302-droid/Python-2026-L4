9613520fcd32fcc831a2c4ab2c4963f48abfd159
hieuph2410302-droid <hieuph2410302@usth.edu.vn>
2026-09-25 16:32:03 +0700

    Delete ex9.py

--- ex9.py
+++
@@ -1,8 +0,0 @@
-def calculate_factorial(n):
-    result = 1
-    for i in range(1, n + 1):
-        result = result * i
-    return result
-number = int(input("Enter a positive integer: "))
-factorial = calculate_factorial(number)
-print(f"The factorial of {number} is: {factorial}")
